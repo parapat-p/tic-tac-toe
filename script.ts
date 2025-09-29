c55fa6ec71e49e02e7d690422b398503ab85f95e
parapat-p <parapat.patc@gmail.com>
2025-09-29 22:05:00 +0700

    Add tic tac toe check win condition logic'

--- script.ts
+++ script.ts
@@ -6,6 +6,7 @@ type PlayerInstance = {
 
 type GridArrayInstance = {
     addPlayerMark: (playerMark:string,positionX:number,positionY:number) => void;
+    getGridValue: (positionX:number,positionY:number) => string;
 }
 
 type GameInstance = {
@@ -54,16 +55,69 @@ const GridArrayManager = ():GridArrayInstance => {
         gridArray[positionX][positionY] = playerMark;
     }
 
+    const getGridValue = (positionX:number,positionY:number) => {
+        return gridArray[positionX][positionY];
+    }
+
     return {
+        getGridValue,
         addPlayerMark
     }
 }
 
 
-const tictactoeGame = (p1:PlayerInstance,p2:PlayerInstance):GameInstance => {
+const tictactoeGame = (player1:PlayerInstance,player2:PlayerInstance):GameInstance => {
     
+    const p1 = player1;
+    const p2 = player2;
+
     const p1Mark = 'x';
     const p2Mark = 'o';
+    let gameRound = 0;
+    const gameGrid = GridArrayManager();
+
+    const checkWinning = () => {
+        // check column
+        let gameOver:boolean = false;
+        for(let column = 0;column<3;column++){
+            gameOver = checkValidWin(gameGrid.getGridValue(0,column),gameGrid.getGridValue(1,column),gameGrid.getGridValue(2,column));
+            if(gameOver){
+                return gameGrid.getGridValue(0,column)
+            }
+        }
+        
+        for(let row = 0;row<3;row++){
+            let gameOver = checkValidWin(gameGrid.getGridValue(row,0),gameGrid.getGridValue(row,1),gameGrid.getGridValue(row,2));
+            if(gameOver){
+                return gameGrid.getGridValue(row,0)
+            }
+        }
+
+        gameOver = checkValidWin(gameGrid.getGridValue(0,0) ,gameGrid.getGridValue(1,1) ,(gameGrid.getGridValue(2,2)) )
+        gameOver = checkValidWin(gameGrid.getGridValue(0,2) ,gameGrid.getGridValue(1,1) ,(gameGrid.getGridValue(2,0)) )
+        if(gameOver){
+            return gameGrid.getGridValue(1,1)
+        }
+
+
+        return "";
+
+
+    }
+
+
+    const checkValidWin = (first:string,second:string,third:string):boolean => {
+        
+        if( ( first === "" ) || ( second === "" ) || ( third === "") ){
+            return false
+        }
+
+        if( !( ( first === second ) && ( second === third ) ) ){
+            return false
+        }
+
+        return true;
+    }
 
     return {
 
